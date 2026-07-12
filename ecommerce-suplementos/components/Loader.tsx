ae564e4412012be29157604fc5eb07b76c0991db
victorcolmenaresmoa-max <victorcolmenares.moa@gmail.com>
2026-07-12 19:37:05 -0400

    Delete ecommerce-suplementos directory

--- ecommerce-suplementos/components/Loader.tsx
+++
@@ -1,8 +0,0 @@
-export default function Loader({ label }: { label?: string }) {
-  return (
-    <div className="loading-block">
-      <div className="spinner spinner-lg" />
-      <span>{label || 'Cargando...'}</span>
-    </div>
-  );
-}
